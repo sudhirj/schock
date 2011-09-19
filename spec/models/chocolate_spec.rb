0356a38371ae495f28d72073ed50ae5f3ffae6e7
Sudhir Jonathan <sudhir.j@gmail.com>
2011-09-20 02:40:13 +0530

    refactored the chocolate model tests

--- spec/models/chocolate_spec.rb
+++ spec/models/chocolate_spec.rb
@@ -1,19 +1,8 @@
 require 'spec_helper'
 
 describe Chocolate do
-  it "fails validation with no name" do
-    Chocolate.new.should have(1).errors_on(:name)
-  end
-  
-  it "fails validation with no desciption" do
-    Chocolate.new.should have(2).errors_on(:desc)    
-  end
-  
-  it "fails validation with very short descriptions" do
-    Chocolate.new(:desc => 'A').should have(1).errors_on(:desc)
-  end
-  
-  it "fails when no prices are given" do
-    Chocolate.new.should have(1).error_on(:price)
-  end
+  it {should have(1).errors_on(:name)}
+  it {should have(2).errors_on(:desc)}
+  it {Chocolate.new(:desc => 'A').should have(1).errors_on(:desc)}  
+  it {should have(1).error_on(:price)}
 end
