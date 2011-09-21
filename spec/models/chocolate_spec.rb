156cbf1a75220af1e92106d196c17cdb6fb93c15
Sudhir Jonathan <sudhir.j@gmail.com>
2011-09-21 21:21:29 +0530

    added chocolate grid

--- spec/models/chocolate_spec.rb
+++ spec/models/chocolate_spec.rb
@@ -4,5 +4,5 @@ describe Chocolate do
   it {should have(1).errors_on(:name)}
   it {should have(2).errors_on(:desc)}
   it {Chocolate.new(:desc => 'A').should have(1).errors_on(:desc)}  
-  it {should have(1).error_on(:price)}
+  it {should have(1).error_on(:price)}  
 end
